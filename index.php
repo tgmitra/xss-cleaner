6652dd0c342aa86ea1d6d8aec2b048dfa6fb4ea2
Tathagata <tathagata@ishippo.com>
2015-12-09 16:10:35 +0530

    Add example code

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
+
+# Include security class
 include 'security.class.php';
 
+# Initiate filter_payload
 $filter_payload = new filter_payload();
+
+# Clean all request payload
 $filter_payload->clean_request_payload();
 
